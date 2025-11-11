e2813be9994063f4f5d91b840555ed80110ec1c0
IranMatheus <iranmrr090379@gmail.com>
2025-11-11 22:49:59 +0800

    Delete aula 1 directory

--- aula 1/certoOuErrado.js
+++
@@ -1,9 +0,0 @@
-function verificarResposta(respostaUsuario) {
-  let respostaCorreta = "certo";
-
-  if (respostaUsuario === respostaCorreta) {
-    alert("Correto! A forma correta de declarar uma variável é com 'var'.");
-  } else {
-    alert("Errado! A forma correta de declarar uma variável é com 'var'.");
-  }
-}
