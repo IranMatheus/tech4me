41db8bcfabdeffeab5fe9d7b3f5e06348821f898
IranMatheus <iranmrr090379@gmail.com>
2025-11-11 22:49:31 +0800

    Delete aula 11 directory

--- aula 11/chamada/chamada.js
+++
@@ -1,8 +0,0 @@
-function listaCor() {
-    var nome = [];
-
-    for (var contador = 0; contador < 3; contador++) {
-        nome[contador] = prompt("Bem-vindo à chamada! Qual é o seu nome?");
-    }
-}
-listaCor();
--- aula 11/pushLista.js
+++
@@ -1,9 +0,0 @@
-function addNome() {
-    var nome = ["edson", "gustavo", "pedro"];
-
-    while (nome !== "") {
-        nome.push("gabriela");
-        console.log(nome);
-    }
-}
-addNome();
